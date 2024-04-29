8ed415e1b0f4baade8a2b12c8607418dacfa1d68
pXBnJBHkzrc <168419969+pXBnJBHkzrc@users.noreply.github.com>
2024-04-29 06:04:48 -0500

    Update FloppaHubLoaderAllGames.lua

--- FloppaHubLoaderAllGames.lua
+++ FloppaHubLoaderAllGames.lua
@@ -1,5 +1,6 @@
-local games = {}
 --AL
 
-if game.PlaceId == 10595058975 then
-loadstring(game:HttpGet(‘https://raw.githubusercontent.com/pXBnJBHkzrc/Floppa-Hub-Games/main/FloppaHubAL.lua’))()
+if game.PlaceId == 10595058975 then 
+
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/pXBnJBHkzrc/Floppa-Hub-Games/main/FloppaHubAL.lua'))()
+end
